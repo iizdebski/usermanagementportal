153cb6cceb2197a1acec543badcf7840cfcfb301
SwAppCoder <iizdepskyy112@gmail.com>
2021-12-08 12:10:46 +0200

    user_management_portal

---
+++ supportPortalAngularJWTBackend/supportportal/src/main/java/com/supportportal/constant/EmailConstant.java
@@ -0,0 +1,6 @@
+package com.supportportal.constant;
+
+public class EmailConstant {
+    public static final String SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
+    public static final String SMTP_STARTTLS_REQUIRED = "mail.smtp.starttls.required";
+}
---
+++ supportPortalAngularJWTBackend/supportportal/src/main/java/com/supportportal/service/impl/NotAnImageFileException.java
@@ -0,0 +1,7 @@
+package com.supportportal.service.impl;
+
+public class NotAnImageFileException extends Exception {
+    public NotAnImageFileException(String message) {
+        super(message);
+    }
+}
